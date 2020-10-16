f3bef0ccf5d99c9722b74c06a16893d25b1eddad
Alekhya-97 <70754842+Alekhya-97@users.noreply.github.com>
2020-10-16 19:07:14 +0530

    Add files via upload

---
+++ Assignment Day 2/Employee.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Employee {
+
+     public String name;
+     public int age;
+    public String city;
+
+    public void output(){
+
+        System.out.println(" The name is " +name);
+        System.out.println(" The age is " + age);
+        System.out.println(" The city name is  " +city);
+    }
+}
---
+++ Assignment Day 2/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+            Employee e= new Employee();
+            Employee f= new Employee();
+
+            e.name="Alekhya";
+            e.age=23;
+            e.city="Vizag";
+            e.output();
+
+            f.name="Aahana";
+            f.age=23;
+            f.city= "Mumbai";
+            f.output();
+
+        }
+    }
+
